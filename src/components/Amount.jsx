2549498261da8eb2120d37e747295fe0195396b1
Rob Brennan <rob@therobbrennan.com>
2018-08-22 11:32:25 -0700

    State the problem. How are we going to use the value from the Amount component in currency components?

--- src/components/Amount.jsx
+++ src/components/Amount.jsx
@@ -22,6 +22,17 @@ class Amount extends Component {
         <span>US Dollar: {this.state.amount}</span>
         <button type="button" onClick={this.onIncrement}>+</button>
         <button type="button" onClick={this.onDecrement}>-</button>
+
+        {/* 
+          Imagine a scenario where you have currency components that need to use the value of amount.
+
+          One naive approach is to simple render the currency components within Amount: 
+          
+          <Euro amount={this.state.amount} />
+          <Pound amount={this.state.amount} />
+          
+          You could render components within Amount for simplicity. If there is no strong use case for having more control about what is rendered in Amount, this may be fine. However...the downside is that the Amount component now has to know about the currency components. Everytime the rendered output needs to be changed, the Amount component would be touched. What about leaving the component as it was and get the currency components outside of it?
+        */}
       </div>
     )
   }
