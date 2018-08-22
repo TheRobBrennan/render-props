5b2b390df4f4bf13104047a015909a4edd937458
Rob Brennan <rob@therobbrennan.com>
2018-08-22 11:40:48 -0700

    One solution: Lift state from the Amount component into the App component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
 // Sample project for the tutorial at https://www.robinwieruch.de/react-render-props-pattern/
-import React from 'react'
+import React, { Component } from 'react'
 import Amount from './components/Amount'
 
-const App = () => <Amount />
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      amount: 0,
+    }
+  }
+
+  onIncrement = () => {
+    this.setState(state => ({ amount: state.amount + 1 }))
+  }
+
+  onDecrement = () => {
+    this.setState(state => ({ amount: state.amount - 1}))
+  }
+
+  render(){
+    return (
+      <div>
+        <Amount amount={this.state.amount} onIncrement={this.onIncrement} onDecrement={this.onDecrement} />
+        {/* 
+          Imagine a scenario where you have currency components that need to use the value of amount.
+
+          This approach will use lifting state to the parent component to render the currency components using the value in our Amount component: 
+          
+          <Euro amount={this.state.amount} />
+          <Pound amount={this.state.amount} />
+          
+          This is a valid solution. The App component has turned into a stateful class component, and the Amount component is now a functional stateless component.
+        */}
+      </div>
+    )
+  }
+}
 
 export default App
--- src/components/Amount.jsx
+++ src/components/Amount.jsx
@@ -1,41 +1,15 @@
 import React, { Component } from 'react'
 
-class Amount extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      amount: 0,
-    }
-  }
-
-  onIncrement = () => {
-    this.setState(state => ({ amount: state.amount + 1 }))
-  }
-
-  onDecrement = () => {
-    this.setState(state => ({ amount: state.amount - 1}))
-  }
-
-  render () {
-    return (
-      <div>
-        <span>US Dollar: {this.state.amount}</span>
-        <button type="button" onClick={this.onIncrement}>+</button>
-        <button type="button" onClick={this.onDecrement}>-</button>
-
-        {/* 
-          Imagine a scenario where you have currency components that need to use the value of amount.
-
-          One naive approach is to simple render the currency components within Amount: 
-          
-          <Euro amount={this.state.amount} />
-          <Pound amount={this.state.amount} />
-          
-          You could render components within Amount for simplicity. If there is no strong use case for having more control about what is rendered in Amount, this may be fine. However...the downside is that the Amount component now has to know about the currency components. Everytime the rendered output needs to be changed, the Amount component would be touched. What about leaving the component as it was and get the currency components outside of it?
-        */}
-      </div>
-    )
-  }
-}
+const Amount = ({ amount, onIncrement, onDecrement }) => (
+  <div>
+    <span>US Dollar: {amount}</span>
+    <button type="button" onClick={onIncrement}>
+      +
+    </button>
+    <button type="button" onClick={onDecrement}>
+      -
+    </button>
+  </div>
+)
 
 export default Amount
