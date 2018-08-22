a98adf3308e1c5cade9bac5b06097e3078cd5bc4
Rob Brennan <rob@therobbrennan.com>
2018-08-22 12:20:00 -0700

    Note how we can add additional components and elements into the composition function

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ const App = () => (
   <Amount>
     {amount => (
       <div>
+        <h1>My Currency Converter</h1>
         <Pound amount={amount} />
         <Euro amount={amount} />
       </div>
